a123cddce732029b0d280cdf2cd36101f4603149
Mimbsy23 <elviscobright@gmail.com>
2023-12-19 14:23:20 -0800

    sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - This sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int temp;
+
+        if (size < 2)
+                return;
+
+        for (i = 0; i < size; i++)
+        {
+                for (j = 0; j + 1 < size; j++)
+                {
+                        if (array[j] > array[j + 1])
+                        {
+                                /* Swap elements if they are in the wrong order */
+                                temp = array[j + 1];
+                                array[j + 1] = array[j];
+                                array[j] = temp;
+
+                                /* Print array after each swap */
+                                print_array(array, size);
+                        }
+                        /* Continue to the next iteration */
+                        continue;
+                }
+        }
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ *                   using the selection sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int min_val;
+        size_t idx;
+
+        if (size < 2)
+                return;
+        for (i = 0; i < size - 1; i++)
+        {
+                idx = i;        /* Assume the current index has the minimum value */
+                min_val = array[i]; /* Initialize minimum value */
+                j = i + 1;
+
+                for (; j < size; j++)
+                {
+                        if (array[j] < min_val)
+                        {
+                                min_val = array[j];
+                                idx = j;
+                        }
+                }
+
+                /* Swap the minimum value with the first element if needed */
+                array[idx] = array[i];
+                if (array[i] != min_val)
+                {
+                        array[i] = min_val;
+                        print_array(array, size); /* Print the array after each swap */
+                }
+        }
+}
