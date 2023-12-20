2240387b89736da45da8f08fdcac19ca9e74f35f
Soledadkesi <soledadkesi1995@gmail.com>
2023-12-20 04:14:35 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int temp;
+	size_t i, j;
+	int temp;
 
-        if (size < 2)
-                return;
+	if (size < 2)
+		return;
 
-        for (i = 0; i < size; i++)
-        {
-                for (j = 0; j + 1 < size; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                /* Swap elements if they are in the wrong order */
-                                temp = array[j + 1];
-                                array[j + 1] = array[j];
-                                array[j] = temp;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j + 1 < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap elements if they are in the wrong order */
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
 
-                                /* Print array after each swap */
-                                print_array(array, size);
-                        }
-                        /* Continue to the next iteration */
-                        continue;
-                }
-        }
+				/* Print array after each swap */
+				print_array(array, size);
+			}
+			/* Continue to the next iteration */
+			continue;
+		}
+	}
 }
