c7b59e932c88fc1f073d60a676f2e590902309e7
Mimbsy23 <elviscobright@gmail.com>
2023-12-21 00:31:12 -0800

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,33 +10,33 @@
  */
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int min_val;
-        size_t idx;
+	size_t i, j;
+	int min_val;
+	size_t idx;
 
-        if (size < 2)
-                return;
-        for (i = 0; i < size - 1; i++)
-        {
-                idx = i;        /* Assume the current index has the minimum value */
-                min_val = array[i]; /* Initialize minimum value */
-                j = i + 1;
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		idx = i;        /* Assume the current index has the minimum value */
+		min_val = array[i]; /* Initialize minimum value */
+		j = i + 1;
 
-                for (; j < size; j++)
-                {
-                        if (array[j] < min_val)
-                        {
-                                min_val = array[j];
-                                idx = j;
-                        }
-                }
+		for (; j < size; j++)
+		{
+			if (array[j] < min_val)
+			{
+				min_val = array[j];
+				idx = j;
+			}
+		}
 
-                /* Swap the minimum value with the first element if needed */
-                array[idx] = array[i];
-                if (array[i] != min_val)
-                {
-                        array[i] = min_val;
-                        print_array(array, size); /* Print the array after each swap */
-                }
-        }
+		/* Swap the minimum value with the first element if needed */
+		array[idx] = array[i];
+		if (array[i] != min_val)
+		{
+			array[i] = min_val;
+			print_array(array, size); /* Print the array after each swap */
+		}
+	}
 }
